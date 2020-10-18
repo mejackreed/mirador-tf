3c36746dafe6c8ab5935ffba4622ea19b1111a74
Jack Reed <phillipjreed@gmail.com>
2020-10-18 14:16:02 -0600

    Use updated catalog syntax

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ import TensorFlowTool from './TensorFlowTool';
 
 Mirador.viewer({
   id: 'mirador',
-  manifests: {
-    'https://purl.stanford.edu/dq881jb6359/iiif/manifest': {},
-    'https://purl.stanford.edu/hg676jb4964/iiif/manifest': {},
-    'https://cdm16453.contentdm.oclc.org/iiif/info/p15426coll7/15435/manifest.json': {},
-    'https://cdm16079.contentdm.oclc.org/iiif/info/p16079coll32/391570/manifest.json': {},
-    'https://cdm16876.contentdm.oclc.org/iiif/info/p16876coll1/8491/manifest.json': {}
-  },
+  catalog: [
+    { manifestId: 'https://purl.stanford.edu/dq881jb6359/iiif/manifest' },
+    { manifestId: 'https://purl.stanford.edu/hg676jb4964/iiif/manifest' },
+    { manifestId: 'https://cdm16453.contentdm.oclc.org/iiif/info/p15426coll7/15435/manifest.json' },
+    { manifestId: 'https://cdm16079.contentdm.oclc.org/iiif/info/p16079coll32/391570/manifest.json' },
+    { manifestId: 'https://cdm16876.contentdm.oclc.org/iiif/info/p16876coll1/8491/manifest.json' }
+  ],
   window: {
     defaultSideBarPanel: 'annotations',
     sideBarOpenByDefault: true,
